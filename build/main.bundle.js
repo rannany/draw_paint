52212f5330db2c75dd3c8ebe96bebbdd9e6ab37c
rannany <rannany@gmail.com>
2018-04-18 20:52:20 -0300

    primeiro commit

---
+++ build/main.bundle.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function myFoo() {
+  console.log('myFoo');
+  var teste = 'da';
+}
+
+function main() {
+  console.log('teste');
+  myFoo();
+}
---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+function myFoo () {
+  console.log('myFoo');
+  let teste = 'da';
+}
+
+function main() {
+  console.log('teste');
+  myFoo();
+}
