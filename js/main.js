7170183cc9521e920946fdaf916da4ca15a51d0f
rannany <rannany@gmail.com>
2018-04-19 01:15:32 -0300

    criada contexto do canvas e arquivo de estilo

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
+(() => {
+  let canvas = document.getElementById('canvas'),
+      context = canvas.getContext('2d');
 
-function myFoo () {
-  console.log('myFoo');
-  let teste = 'da';
-}
-
-function main() {
-  console.log('teste');
-  myFoo();
-}
+  context.fillStyle = "rgb(200,0,0)";
+  context.fillRect (10, 10, 55, 50);
+  context.fillStyle = "rgba(0, 0, 200, 0.5)";
+  context.fillRect (30, 30, 55, 50);
+})();
